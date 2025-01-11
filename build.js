36d046fb467f70ae030afc5eb3387f243e641416
Lluis Ingles <lluis.ingles@exoticca.com>
2025-01-11 14:02:09 +0100

    add robots and sitemap

--- build.js
+++ build.js
@@ -36,10 +36,48 @@ async function build() {
 
       console.log(`Built ${lang} version`);
     }
+
+    // Copy static files to root directory
+    await copyStaticFiles();
   } catch (error) {
     console.error('Build failed:', error);
     process.exit(1);
   }
+}
+
+async function copyStaticFiles() {
+  // Copy to root directory instead of dist
+  await fs.writeFile('robots.txt', 
+    'User-agent: *\n' +
+    'Allow: /\n' +
+    'Allow: /ca/\n\n' +
+    'Sitemap: https://lluisingles.com/sitemap.xml\n'
+  );
+
+  await fs.writeFile('sitemap.xml',
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n' +
+    '        xmlns:xhtml="http://www.w3.org/1999/xhtml">\n' +
+    '  <url>\n' +
+    '    <loc>https://lluisingles.com/</loc>\n' +
+    '    <lastmod>2024-03-21</lastmod>\n' +
+    '    <changefreq>monthly</changefreq>\n' +
+    '    <priority>1.0</priority>\n' +
+    '    <xhtml:link rel="alternate" hreflang="en" href="https://lluisingles.com/"/>\n' +
+    '    <xhtml:link rel="alternate" hreflang="ca" href="https://lluisingles.com/ca/"/>\n' +
+    '  </url>\n' +
+    '  <url>\n' +
+    '    <loc>https://lluisingles.com/ca/</loc>\n' +
+    '    <lastmod>2024-03-21</lastmod>\n' +
+    '    <changefreq>monthly</changefreq>\n' +
+    '    <priority>0.9</priority>\n' +
+    '    <xhtml:link rel="alternate" hreflang="en" href="https://lluisingles.com/"/>\n' +
+    '    <xhtml:link rel="alternate" hreflang="ca" href="https://lluisingles.com/ca/"/>\n' +
+    '  </url>\n' +
+    '</urlset>'
+  );
+
+  console.log('Generated static files');
 }
 
 build(); 
